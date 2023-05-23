e2c0c304b8710ff55ba460e1c8e97fec3946bb8d
Aleksandar <skale05@yahoo.com>
2023-05-23 17:32:22 +0200

    edit axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
+import axios from "axios";
+import PostsList from './components/PostsList';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([]);
+  const url=("https://jsonplaceholder.typicode.com");
+  
+  axios.get(url) 
+  .then(function(response) {
+    const result=response
+    setPosts(result);
+
+    })
+    
+
+
+.catch(function(error){
+  console.log(error);
+})
+.finally(function(){});
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <PostsList data={posts}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
